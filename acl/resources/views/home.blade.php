7f982ce491c8f24d1769ef8d49be10ce22162f5e
diztop <roman.palamar@gmail.com>
2020-10-05 21:20:42 +0300

    update view

---
+++ acl/resources/views/home.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-body">
+
+                    @if (Session::get('error_message'))
+                        <p>{{ Session::get('error_message') }}</p>
+                    @else
+                        <p>{{ __('Home page for logined sub user') }}</p>
+                    @endif
+
+
+
+
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
